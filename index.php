a063cdfcbd7993b0590eeb2bd429ce4c0f3e3746
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-26 23:00:24 -0700

    Acheived fixed and absolute positioned, nested divs.

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="src/css/style.css">
   <title>Tuff Tomatoes Test Site</title>
 </head>
 <body>
-  
+
+  <section id="home" data-type="background" data-speed="10">
+
+      <div class="another">
+        <div class="yet-another"></div>
+        <div class="again-another"></div>
+      </div>
+  </section>
+
+<section id="about" data-type="background" data-speed="10">
+  <article>Simple Parallax Scroll</article>
+</section>
+
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </body>
 </html>
