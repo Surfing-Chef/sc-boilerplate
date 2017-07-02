c9fd9690d5c0eff4565854fbafc5a81a9c7eca05
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-02 15:25:42 -0700

    Folder containing files to process

---
+++ process/js/script.js
@@ -0,0 +1,11 @@
+function f() {
+  {
+    var x;
+    {
+      // this is ok since it's a block scoped name
+      var x = "sneaky";
+    }
+    // this is ok since it was declared with `let`
+    x = "bar";
+  }
+}
