d681847ec1b29ccbb0e72fc26ffed18c43b9273c
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-02 15:44:25 -0700

    Changed test function to placeholder

--- process/js/script.js
+++ process/js/script.js
@@ -1,11 +1,1 @@
-function f() {
-  {
-    var x;
-    {
-      // this is ok since it's a block scoped name
-      var x = "sneaky";
-    }
-    // this is ok since it was declared with `let`
-    x = "bar";
-  }
-}
+// Place functions in this folder to process for builds.
