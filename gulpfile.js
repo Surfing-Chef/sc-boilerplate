bd3b0da69b9715043d770d18adecad6a2b54aa86
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-09 12:40:59 -0700

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 // REQUIRED
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    uglify = require('gulp-uglify'),
-    plumber = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    del = require('del'),
-    rename = require('gulp-rename'),
-    babel = require('gulp-babel'),
-    imagemin = require('gulp-imagemin');
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    browserSync   = require('browser-sync').create(),
+    uglify        = require('gulp-uglify'),
+    plumber       = require('gulp-plumber'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    concat        = require('gulp-concat'),
+    del           = require('del'),
+    rename        = require('gulp-rename'),
+    babel         = require('gulp-babel'),
+    imagemin      = require('gulp-imagemin');
 
 // DEVELOPMENT TASKS
 // Scripts Task - tasks related to js
@@ -27,7 +27,7 @@ gulp.task('scripts', function(){
 
 // Sass Task - development css - nested/readable/mapped
 gulp.task('sassDev', function() {
-  gulp.src('process/scss/**/*.scss')
+  gulp.src('process/sass/**/*.scss')
   .pipe(plumber())
   .pipe(sourcemaps.init())
     .pipe(sass({sourceComments: 'map', sourceMap: 'sass', outputStyle: 'expanded'}))
@@ -40,11 +40,11 @@ gulp.task('sassDev', function() {
 // Server Task - Asynchronous browser syncing of assets across multiple devices
 gulp.task('serve', function(){
   browserSync.init({
-    proxy   : "http://localhost/app-root/app-file" // update this path to project root
+    proxy   : "http://localhost/path/to/root" // update this path to project root
   });
 
   gulp.watch('process/js/**/*.js', ['scripts']);
-  gulp.watch('process/scss/**/*.scss', ['sassDev']);
+  gulp.watch('process/sass/**/*.scss', ['sassDev']);
   gulp.watch('**/*.html').on('change', browserSync.reload);
   gulp.watch('**/*.php').on('change', browserSync.reload);
 });
@@ -57,49 +57,56 @@ gulp.task('default', ['serve']);
 // delete previous production build
 gulp.task('prod:cleanfolder', function(){
   return del([
-    'builds/prod/**/*'
+    './prod/**/*'
   ]);
 });
 
 // minimize images
 gulp.task('prod:imgMin', ['prod:cleanfolder'], function(){
-    return gulp.src('dev/img/**/*')
+    return gulp.src('./img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('builds/prod/img'));
+        .pipe(gulp.dest('./prod/img'));
 });
 
 // minify css
 gulp.task('prod:sass', ['prod:cleanfolder'], function() {
-  gulp.src('process/scss/style.scss')
+  gulp.src('process/sass/style.scss')
   .pipe(plumber())
   .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(gulp.dest('builds/prod/css/'));
+  .pipe(gulp.dest('./prod'));
 });
 
 // uglify and mangle js
 gulp.task('prod:scripts', ['prod:sass'], function(){
-  gulp.src(['builds/dev/js/**/*.js'])
+  gulp.src(['./js/**/*.js'])
   .pipe(plumber())
   .pipe(uglify())
-  .pipe(gulp.dest('builds/prod/js'));
+  .pipe(gulp.dest('./prod/js'));
 });
 
 // copy development files not requiring processing
 gulp.task('prod:copy', ['prod:imgMin'], function(){
   return gulp.src([
-                  'builds/dev/**/*/',
-                  '!builds/dev/css{,/**}',
-                  '!builds/dev/js{,/**}',
-                  '!builds/dev/img{,/**}'
+                  './**/*/',
+                  // '!builds/dev/css{,/**}',
+                  '!./node_modules{,/**}',
+                  '!./prod{,/**}',
+                  '!./process{,/**}',
+                  '!./js{,/**}',
+                  '!./img{,/**}',
+                  '!./*.md',
+                  '!./gulpfile.js',
+                  '!./package.json',
+                  '!./style.css'
                 ])
-  .pipe(gulp.dest('./builds/prod'));
+  .pipe(gulp.dest('./prod'));
 });
 
 // remove unwanted build files and directories
 gulp.task('prod:remove', ['prod:copy'], function(done){
   return del([  // list files and directories to delete
     // list of sneaky files to delete
-    // 'builds/prod/target.xxx'
+    // './prod/target.xxx'
   ], done);
 });
 
